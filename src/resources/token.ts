8c672836d904563b0319002ccd3f6142ae1237b4
fatimaaurangzeb0640 <fatimaaurangzeb0640@gmail.com>
2023-09-20 10:22:07 +0500

    removed unused code

--- src/resources/token.ts
+++ src/resources/token.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { PaymentsCreateData, SafepayConfigSubscriptions } from '../types'
+import { SafepayConfigSubscriptions } from '../types'
 import { buildApiUrl } from '../utils/builder'
 import { SubscriptionCheckoutCreateData } from '../types/subscription'
 
